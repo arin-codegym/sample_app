779afa9d3565143c40d84fd105db64e54644040f
arin-codegym <caoquochuy21@gmail.com>
2021-01-04 12:14:57 +0700

    Chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-
   get "/about", to: "static_pages#about"
   get "/help", to: "static_pages#help"
+  root "static_pages#home"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users, only: %i(create show)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
